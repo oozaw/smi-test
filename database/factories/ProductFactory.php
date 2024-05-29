6e1640e46e9ae31338378cfc50046e67f042c3a3
oozaw <79121990+oozaw@users.noreply.github.com>
2024-05-29 20:06:33 +0700

    setup database seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,17 +7,19 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
  */
-class ProductFactory extends Factory
-{
+class ProductFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
         return [
-            //
+            'category_id' => $this->faker->numberBetween(1, 10),
+            'name' => $this->faker->sentence(3),
+            'buy_price' => $this->faker->randomFloat(2, 1, 1000),
+            'sell_price' => $this->faker->randomFloat(2, 1, 1000),
+            'stock' => $this->faker->numberBetween(1, 100),
         ];
     }
 }
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,13 +5,26 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class CategorySeeder extends Seeder
-{
+class CategorySeeder extends Seeder {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        //
+    public function run(): void {
+        $categories = [
+            'Electronics',
+            'Clothing',
+            'Books',
+            'Toys',
+            'Furniture',
+            'Food',
+            'Drinks',
+            'Stationery',
+            'Tools',
+            'Sporting Goods',
+        ];
+
+        foreach ($categories as $category) {
+            \App\Models\Category::factory()->create(['name' => $category]);
+        }
     }
 }
